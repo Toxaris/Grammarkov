49d4a2041c28dea42350509a9ea4b533f97fd429
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2013-04-18 11:44:29 +0200

    Document Text.Grammarkov.

--- src/Text/Grammarkov.hs
+++ src/Text/Grammarkov.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE GADTs, GeneralizedNewtypeDeriving #-}
+-- | The Grammarkov embedded domain-specific language for
+-- nondeterministically building a sequence.
 module Text.Grammarkov
-  ( Grammarkov
+  ( -- * Describing Sequences
+    Grammarkov
   , say
   , (<|>)
   , choose
+    -- * Executing Sequence Descriptions
   , generateAll
   , generateRandom
   ) where
@@ -14,6 +18,8 @@ import Data.String
 
 import System.Random
 
+-- | The Grammarkov monad for nondeterministically building
+-- a sequence. The elements of sequences are of type 'e'.
 newtype Grammarkov e a = Grammarkov (Program (Instruction e) a)
   deriving (Functor, Monad)
 
@@ -24,15 +30,22 @@ data Instruction e a where
   Say :: e -> Instruction e ()
   Choose :: [Program (Instruction e) a] -> Instruction e a
 
+-- | Append an element to the sequence.
 say :: e -> Grammarkov e ()
 say x = Grammarkov (singleton (Say x))
 
+-- | Choose nondeterministically from a list of Grammarkov
+-- sequence builders.
 choose :: [Grammarkov e a] -> Grammarkov e a
 choose ps = Grammarkov (singleton (Choose [p | Grammarkov p <- ps]))
 
+-- | Choose nondeterministically from two Grammarkov sequence
+-- builders.
 (<|>) :: Grammarkov e a -> Grammarkov e a -> Grammarkov e a
 p <|> q = choose [p, q]
 
+-- | Generate all sequences described by a Grammarkov sequence
+-- builder.
 generateAll :: Grammarkov e a -> [[e]]
 generateAll (Grammarkov p) = go p where
   go p = case view p of
@@ -40,6 +53,8 @@ generateAll (Grammarkov p) = go p where
     Say x :>>= k -> fmap (x :) (go (k ()))
     Choose ps :>>= k -> ps >>= \p -> go (p >>= k)
 
+-- | Generate one sequence described by a Grammarkov sequence
+-- builder. Nondeterministic choices are resolved randomly.
 generateRandom :: Grammarkov e a -> IO [e]
 generateRandom (Grammarkov p) = go p where
   go p = case view p of
